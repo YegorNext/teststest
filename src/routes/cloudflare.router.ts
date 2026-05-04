36394b79fa8074f135975df3db714f9c0129e5d1
YegorNext <yegor.d2015@gmail.com>
2026-05-04 20:31:42 +0300

    t2

---
+++ src/routes/cloudflare.router.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { CloudflareController } from "../controllers/cloudflare.controller";
+import { errorHandler } from "../utils/handlers/error.handler";
+
+const router = Router();
+
+const controller = new CloudflareController();
+
+router.post("/provision", async (req, res, next) => {
+  try {
+    await controller.provisionDomain(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use(errorHandler);
+
+export default router;
